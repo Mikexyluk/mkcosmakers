cc3bc91ba00009cdec74f81ea3c022bfd7677a4e
BRUNA LETICIA DOS SANTOS CLEMENTE <bruna.lsclemente@senacsp.edu.br>
2025-06-27 17:19:17 -0300

    feat(home): att1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 const Home = () => {
   return ( 
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <p className="text-lg text-gray-700">
-        Welcome to my Next.js application.
-      </p>
+      <div className="min-h-screen bg-pink-200 flex flex-col items-center justify-start">
+        <div className="w-full bg-pink-500 py-8 flex justify-center shadow-md">
+          <h1 className="text-4xl font-extrabold text-white">Bem-vindo ao Site Sabrina Nail Designer</h1>
+        </div>
+        <div className="mt-16 text-center">
+          <h2 className="text-2xl font-bold text-pink-700 mb-4">Site em andamento</h2>
+          <p className="text-lg text-pink-800">
+        Estamos trabalhando para trazer novidades em breve.<br />
+        Fique ligado!
+          </p>
+        </div>
+      </div>
     </>
    );
 }
